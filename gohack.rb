2e09206b1d83e63e2d4eebef2ae77eec1cf18424
Syaefulloh Arnas <syaefulanas121@gmail.com>
2024-05-18 22:46:48 +0700

    update version encrypt shell

--- gohack.rb
+++ gohack.rb
@@ -1,7 +1,7 @@
 class Gohack < Formula
   desc "Go hack to easy make golang api"
   homepage "https://github.com/syaeful16/go_script_shell"
-  url "https://github.com/syaeful16/go_script_shell/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/syaeful16/go_script_shell/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "2837853890b5244069f6599e7ee114f144468fe02894ef26a698b4e89b40a82d"
   license ""
 
@@ -9,7 +9,7 @@ class Gohack < Formula
   depends_on "bash" => :run
 
   def install
-    bin.install "bin/gohack.sh" => "gohack"
+    bin.install "bin/gohack.sh.x" => "gohack"
     # system "./configure", "--disable-silent-rules", *std_configure_args
   end
 
