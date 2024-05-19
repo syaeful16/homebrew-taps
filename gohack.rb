4329f90b44c8a2bdd4c2ec7c5671d9187bcb1ca5
Syaefulloh Arnas <92679271+syaeful16@users.noreply.github.com>
2024-05-19 15:10:10 +0700

    Update gohack.rb

--- gohack.rb
+++ gohack.rb
@@ -1,7 +1,7 @@
 class Gohack < Formula
   desc "Go hack to easy make golang api"
   homepage "https://github.com/syaeful16/go_script_shell"
-  url "https://github.com/syaeful16/go_script_shell/archive/refs/tags/v1.0.2.tar.gz"
+  url "https://github.com/syaeful16/go_script_shell/archive/refs/tags/v1.0.3.tar.gz"
   sha256 "5def77af09f55bc9a616a4ea980b780304977be81815dd55cc1c527a3d0bebfe"
   license ""
 
